e7233220bf9d7a60b72f5a6c1b41f63ef70c1d98
isamuono <ec2-user@ip-172-31-41-166.us-east-2.compute.internal>
2021-09-13 03:02:01 +0000

    XXXXXXX

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create!(id: '1', family_name: 'celeste', first_name: 'bot', accountName: 'celestebot', email: 'celethread136@gmail.com', password: 'dummy_pass')
-
-Community.create!(id: 'celestebot', user_id: '1', communityName: 'celeste_bot', category: 'その他', subcategory: 'その他', prefecture: '東京都', images: File.open("./app/assets/images/icons/image_default.png"), public: '0')
-
-CommunityParticipant.create!(id: '1', user_id: '1', community_id: 'celestebot', role: '1')
-
-Channel.create!(id: 'celestebot', user_id: '1', community_id: 'celestebot', channelName: 'celeste_channel_bot', color: '#a2efef', public: '2')
-
-Gthread.create!(id: 'celestebot', user_id: '1', community_id: 'celestebot', channel_id: 'celestebot', title: 'celeste_botです！', description: '<p>' + 'こんにちは、celeste_botです！' + '</p>')
-
 categories = Category.create([
   { name: '運動・スポーツ・アウトドア' },
   { name: '文化' },
@@ -466,3 +456,13 @@ ReactionImage.create!([
   { emoji_id: '7', entity_name: 'ng_button', images: File.open("public/reaction_images/symbols/ng_button.png") },
   { emoji_id: '7', entity_name: 'beginners_mark', images: File.open("public/reaction_images/symbols/beginners_mark.png") }
 ])
+
+User.create!(id: '1', family_name: 'celeste', first_name: 'bot', accountName: 'celestebot', email: 'celethread136@gmail.com', password: 'dummy_pass')
+
+Community.create!(id: 'celestebot', user_id: '1', communityName: 'celeste_bot', category: 'その他', subcategory: 'その他', prefecture: '東京都', images: File.open("./app/assets/images/icons/image_default.png"), public: '0')
+
+CommunityParticipant.create!(id: '1', user_id: '1', community_id: 'celestebot', role: '1')
+
+Channel.create!(id: 'celestebot', user_id: '1', community_id: 'celestebot', channelName: 'celeste_channel_bot', color: '#a2efef', public: '2')
+
+Gthread.create!(id: 'celestebot', user_id: '1', community_id: 'celestebot', channel_id: 'celestebot', title: 'celeste_botです！', description: '<p>' + 'こんにちは、celeste_botです！' + '</p>')
