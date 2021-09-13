702c764b856362e469c42a439189ce5e8a4c30b5
isamuono <ec2-user@ip-172-31-41-166.us-east-2.compute.internal>
2021-09-13 02:15:54 +0000

    XXXXXXX

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.smtp_settings = {
-    address: 'celethread136@gmail.com',
+    address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
     user_name: ENV['GMAIL_USERNAME'],
